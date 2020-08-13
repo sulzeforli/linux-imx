3df08e2b212294689c2e98d194c513b87f29dfe0
sulzeforli <dragontoli@gmail.com>
2020-08-13 18:55:35 +0800

    update the devicetree-20200813

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. /opt/fsl-imx-xwayland/4.14-sumo/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+export ARCH=arm
+export CROSS_COMPILE=arm-poky-linux-gnueabi-
+#export CROSS_COMPILE=arm-linux-gnueabihf-
+make distclean
+make imx_mx6dsysd_defconfig
+#make imx_v7_defconfig
+#make menuconfig
+make all -j8
+make zImage
+make dtbs
+
---
+++ deploy_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#sudo rm -r /media/one/Boot\ imx6dl/zImage 
+sudo rm -r /media/one/Boot\ imx6dl/mx6dsysd.dtb 
+#sudo cp -r arch/arm/boot/zImage /media/one/Boot\ imx6dl/
+sudo cp -r arch/arm/boot/dts/mx6dsysd.dtb /media/one/Boot\ imx6dl/
+sync
+
